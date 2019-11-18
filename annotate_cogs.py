d1946a5298c55bfc5140fb73eab5383483b6353f
fhwnmatt <48565818+fhwnmatt@users.noreply.github.com>
2019-11-18 17:40:54 +0100

    Update annotate_cogs.py

--- annotate_cogs.py
+++ annotate_cogs.py
@@ -5,8 +5,8 @@ from collections import Counter
 
 func_categories_file = 'functional_categories.txt'
 
-cog_file = sys.argv[1]
-func_file = sys.stdin #sys.argv[2]
+#cog_file = sys.argv[1]
+#func_file = sys.stdin #sys.argv[2]
 
 # Initialize variables
 category2description = {}
@@ -14,21 +14,27 @@ cog2category = {}
 category2count = Counter()
 my_cogs = set()
 
+# Skip comment lines
+def skip_comments(file):
+    for line in file:
+        if not line.strip().startswith('#'):
+            yield line
 
 # Step 1: Read description of functional categories
 with open(func_categories_file) as fin:
-    pass
-
+    #lines = fin.readlines()
+    for line in skip_comments(fin):
+        print(line)
 
 # Step 2: Read file with OGs of interest
-with open(cog_file) as fin:
-    pass
+#with open(cog_file) as fin:
+ #   pass
 
 
 # Step 3: Read file with functional annotations
 #with open(func_file) as fin:
-for line in func_file:
-    pass
+#for line in func_file:
+  #  pass
 
 
 # Step 4: Count and output the categories for OGs of interest
