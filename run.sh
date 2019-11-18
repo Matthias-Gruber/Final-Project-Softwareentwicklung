1b325b87b05157df3c99886af0709bfd8172c351
fhwnmatt <48565818+fhwnmatt@users.noreply.github.com>
2019-11-18 18:58:03 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ set -euxo pipefail
 
 mkdir -p final_project && cd final_project
 mkdir -p bin data doc results
+mv ../annotate_cogs.py bin/annotate_cogs.py
+mv ../read_members_file.py bin/read_members_file.py
+
 
 ###### Use the eggNOG5.0 database to identify conserved single-copy genes.
 
@@ -21,26 +24,26 @@ archaea_fct=$DATADIR/2157/2157_annotations.tsv.gz
 
 ### 1. Which genes (OGs) occur in at least 99% of all genomes in the eggNOG5 database in each domain of life, respectively?
 
-pwd
-zcat $bacteria | ./../read_members_file.py -min_occurence 99 > data/cogs_bacteria_o99.txt
-zcat $archaea | ./../read_members_file.py -min_occurence 99 > data/cogs_archaea_o99.txt
-zcat $eukaryota | ./../read_members_file.py -min_occurence 99 > data/cogs_eukaryota_o99.txt
+
+zcat $bacteria | bin/read_members_file.py -min_occurence 99 > data/cogs_bacteria_o99.txt
+zcat $archaea | bin/read_members_file.py -min_occurence 99 > data/cogs_archaea_o99.txt
+zcat $eukaryota | bin/read_members_file.py -min_occurence 99 > data/cogs_eukaryota_o99.txt
 
 
 ### 2. Which bacterial genes occur in at least 50% of all bacterial genomes, and in at least 99% thereof as single-copy?
 
-zcat $bacteria | ./read_members_file.py -min_occurence 50 -min_uniqueness 99 > data/cogs_bacteria_o50_u99.txt
+zcat $bacteria | ./bin/read_members_file.py -min_occurence 50 -min_uniqueness 99 > data/cogs_bacteria_o50_u99.txt
 
 # How many of these OGs were also identified as universal bacterial OGs (previous question)?
-comm -12 cogs_bacteria_o99.txt cogs_bacteria_o50_u99.txt
+comm -12 data/cogs_bacteria_o99.txt data/cogs_bacteria_o50_u99.txt
 
 
 ### 3. Identify all OGs that occur as single-copy in at least 97% of all archaea
 
-zcat $archaea | ./read_members_file.py -min_occurence_as_singlecopy 97 > cogs_archaea_os99.txt
+zcat $archaea | ./bin/read_members_file.py -min_occurence_as_singlecopy 97 > data/cogs_archaea_os99.txt
 # Are there archaea which lack 4 or more of those universal OGs?
-zcat $archaea | ./read_members_file.py -min_occurence_as_singlecopy 97 -missing 4
+zcat $archaea | ./bin/read_members_file.py -min_occurence_as_singlecopy 97 -missing 4
 
 
 ### 4. Compile an overview of the functional categories of these 121 archaeal OGs
-zcat $archaea_fct | ./annotate_cogs.py cogs_archaea_os99.txt
+zcat $archaea_fct | ./bin/annotate_cogs.py data/cogs_archaea_os99.txt
