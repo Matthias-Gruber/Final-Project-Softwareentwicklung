acef2ea2fb36182802539194dfbd4acc4635f74d
fhwnmatt <48565818+fhwnmatt@users.noreply.github.com>
2019-11-20 20:19:30 +0100

    Update annotate_cogs.py

--- annotate_cogs.py
+++ annotate_cogs.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-import sys
+import sys, gzip
 from collections import Counter
 
-func_categories_file = 'functional_categories.txt'
+func_categories_file = 'data/functional_categories.txt'
 
-#cog_file = sys.argv[1]
-#func_file = sys.stdin #sys.argv[2]
+cog_file = sys.argv[1]
+#func_file = sys.stdin
+func_file = "/mirror/eggnog/eggnog_5.0/per_tax_level/2157/2157_annotations.tsv.gz"
 
 # Initialize variables
 category2description = {}
@@ -25,16 +26,21 @@ with open(func_categories_file) as fin:
     #lines = fin.readlines()
     for line in skip_comments(fin):
         print(line)
-
+#TODO 
+# dictionary
+# keys: characters
+# values: description
+        
 # Step 2: Read file with OGs of interest
-#with open(cog_file) as fin:
- #   pass
-
+with open(cog_file) as fin:
+    for line in skip_comments(fin):
+        print(line)
 
 # Step 3: Read file with functional annotations
-#with open(func_file) as fin:
-#for line in func_file:
-  #  pass
-
+input = gzip.GzipFile(func_file,'rb')
+i = input.read()
+j = i.decode("utf-8") 
+print(j)
+input.close()
 
 # Step 4: Count and output the categories for OGs of interest
