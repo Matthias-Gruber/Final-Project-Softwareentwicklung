e8b890fe9de4f9322a0fe4671ddf1d3f410d4984
fhwnmatt <48565818+fhwnmatt@users.noreply.github.com>
2019-12-05 13:26:55 +0100

    Update annotate_cogs.py

--- annotate_cogs.py
+++ annotate_cogs.py
@@ -4,9 +4,7 @@ import sys, gzip
 from collections import Counter
 
 func_categories_file = 'data/functional_categories.txt'
-
 cog_file = sys.argv[1]
-#func_file = sys.stdin
 func_file = "/mirror/eggnog/eggnog_5.0/per_tax_level/2157/2157_annotations.tsv.gz"
 
 # Initialize variables
@@ -15,22 +13,15 @@ cog2category = {}
 category2count = Counter()
 my_cogs = set()
 
-# Skip comment lines
-def skip_comments(file):
-    for line in file:
-        if not line.strip().startswith('#'):
-            yield line
-
 # Step 1: Read description of functional categories
 with open(func_categories_file) as fin:
-    #lines = fin.readlines()
-    for line in skip_comments(fin):
-        print(line)
-#TODO 
-# dictionary
-# keys: characters
-# values: description
-        
+    for line in fin:
+        line = line.split(" ")
+        if line[0] == "":
+            key = line[1][1]
+            val = " ".join(line[2:-1])
+            category2description[key] = val
+
 # Step 2: Read file with OGs of interest
 with open(cog_file) as fin:
     for line in skip_comments(fin):
@@ -44,3 +35,4 @@ print(j)
 input.close()
 
 # Step 4: Count and output the categories for OGs of interest
+# Write file categories.txt
